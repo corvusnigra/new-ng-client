985d720143a9bc10dce867c2de12fee624d93bbf
ymarkov <itemruby@gmail.com>
2017-04-28 16:38:58 +0300

    add component ang routings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { HttpModule } from '@angular/http';
 import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { ListEmployeesComponent } from './list-employees/list-employees.component';
+import { EmployeEditComponent } from './employe-edit/employe-edit.component';
+import { EmployeDetailComponent } from './employe-detail/employe-detail.component';
+import { ListDepartmentsComponent } from './list-departments/list-departments.component';
+import { DepartmentEditComponent } from './department-edit/department-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmployeesComponent,
+    EmployeEditComponent,
+    EmployeDetailComponent,
+    ListDepartmentsComponent,
+    DepartmentEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterializeModule
+    MaterializeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employe-detail/employe-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employe-detail',
+  templateUrl: './employe-detail.component.html',
+  styleUrls: ['./employe-detail.component.scss']
+})
+export class EmployeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
