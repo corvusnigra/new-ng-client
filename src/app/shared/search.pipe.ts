676df6afc189144683b60af2c80eece88431bf64
uriy <itemruby@gmail.com>
2017-05-01 03:15:16 +0400

    add filter employees

--- src/app/shared/search.pipe.ts
+++ src/app/shared/search.pipe.ts
@@ -1,4 +1,4 @@
-import { Pipe, PipeTransform, Injectable } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 import {Employe} from "./employe";
 
 @Pipe({
@@ -6,16 +6,18 @@ import {Employe} from "./employe";
     pure: false
 })
 
-@Injectable()
 export class SearchPipe implements PipeTransform {
     transform( employees: Employe[], category: string ): any {
         if (employees == null) {
             return null;
         }
-        if (category && Array.isArray(employees)){
-            return employees.filter(employe => {
-                return employe.lastName.toLowerCase().indexOf(category) !== -1;
-            });
+        if (category && category !== ' ' && Array.isArray(employees) ) {
+            let search  =  category.trim().toLowerCase();
+            if (search) {
+                return employees.filter(employe => {
+                    return employe.lastName.toLowerCase().indexOf(search) !== -1;
+                });
+            }
         }
     }
 }
